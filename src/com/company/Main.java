5f02f29a96641a5ced4720d33e7082e9e36ee197
ujjpatel <ujjavalpatel@hotmail.com>
2021-01-27 23:10:26 +0000

    arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
@@ -15,22 +16,40 @@ public class Main {
         char name = 'A';
         boolean isOnline = true;
         String message = "Hello";
-        System.out.println(message.length());
-
-	    System.out.println("I am a Developer!");
-        System.out.println(age);
-        System.out.println(newAge);
-        System.out.println(message + " Welcome");
+//        System.out.println(message.length());
+//	    System.out.println("I am a Developer!");
+//        System.out.println(age);
+//        System.out.println(newAge);
+//        System.out.println(message + " Welcome");
 
 	    //Reference Types: date,
 
 
         Date todayTime = new Date();
-        System.out.println(todayTime);
+//        System.out.println(todayTime);
 
         Point point1 = new Point(1,1);
         Point point2 = point1;
         point1.x = 33;
-        System.out.println(point2);
+//        System.out.println(point2);
+
+        //Cfreating a Array
+        //Old way of using Array
+        int[] numbers = new int[10];
+        numbers[0] = 1;
+        numbers[9] = 2;
+//        System.out.println(numbers);
+//
+//        System.out.println(Arrays.toString(numbers));
+
+
+        //New way of using array!
+
+
+        int[] newNumbers = {2,3,5,1,4,9};
+        Arrays.sort(newNumbers);
+        System.out.println(newNumbers);
+        System.out.println(Arrays.toString(newNumbers));
+
     }
 }
